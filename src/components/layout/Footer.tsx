927b983a675c1224bc38bbdb4005b6d40f56bcda
Michael-Njogu <mykekunyo@gmail.com>
2025-11-25 22:07:13 +0300

    Convert file extensions from .js/.jsx to .ts/.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -14,7 +14,21 @@ const Footer = () => {
                         <Col md={6}>
                             <ul className="social-icons">
                                 <li><a href="https://www.linkedin.com/in/michael-njogu/" rel="nofollow noreferrer" target="_blank" title="Connect with me on LinkedIn"><Linkedin size={24} /></a></li>
-                                <li><a href="mailto:mykekunyo@gmail.com" rel="nofollow noreferrer" title="Send me an email"><EnvelopeAt size={24} /></a></li>
+                                <li>
+                                    <a
+                                        href="#email"
+                                        onClick={(e) => {
+                                            e.preventDefault()
+                                            const codes = [109, 121, 107, 101, 107, 117, 110, 121, 64, 103, 109, 97, 105, 108, 46, 99, 111, 109]
+                                            const email = String.fromCharCode(...codes)
+                                            window.location.href = `mailto:${email}`
+                                        }}
+                                        title="Send me an email"
+                                        aria-label="Send me an email"
+                                    >
+                                        <EnvelopeAt size={24} />
+                                    </a>
+                                </li>
                                 <li><a href="https://github.com/Michael-K-Njogu" rel="nofollow noreferrer" target="_blank" title="View my GitHub profile"><Github size={24} /></a></li>
                             </ul>
                         </Col>
